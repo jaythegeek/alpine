be7e9901e80460694a127b8f36fb6b9bd8cd70a1
@jaythegeek <alex@jaythegeek.io>
2019-10-28 18:11:36 +0000

    fixes

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer bg-primary mt-auto">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col">
+                <ul class="list-unstyled">
+                    <li>
+                        <a class="text-dark font-weight-bold" href="{{ route('login') }}">{{ __('Candidate Login') }}</a>
+                    </li>
+                    <li>
+                        <a class="text-dark font-weight-bold" href="{{ route('login') }}">{{ __('Staff Login') }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
