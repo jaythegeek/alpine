dc8e469256a5e05308d935b9afeb885a9f1fac34
Alex Jay <alex@jaythegeek.io>
2019-12-08 13:00:48 +0000

    analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,7 +19,17 @@
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154118968-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154118968-1');
+    </script>
 </head>
+
 <body class="h-100">
     <div id="app" class="d-flex flex-column h-100">
         @include('layouts.nav')
@@ -30,4 +41,5 @@
         @include('layouts.footer')
     </div>
 </body>
+
 </html>
